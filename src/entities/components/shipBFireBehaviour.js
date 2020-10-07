7babaf2100685bf139cd1f3eecf580129f89a3dd
Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
2020-10-07 18:48:14 -0300

    Implemented ShipB

---
+++ src/entities/components/shipBFireBehaviour.js
@@ -0,0 +1,72 @@
+import { GameObject } from '../gameobject';
+import { AudioManager } from '../../audios/AudioManager';
+import { Bullet } from '../bullet';
+
+export class ShipBFireBehaviour extends GameObject
+{
+    constructor(gameobject, game, firingSpeed, bulletSpeed, bulletTag, bulletTargetTag) {
+        super(game);
+        this.gameobject = gameobject;
+
+        this.isFiring = false;
+        this.lastFireTime = 0;
+        this.firingSpeed = firingSpeed;
+        this.bulletSpeed = bulletSpeed;
+        this.bulletTag = bulletTag;
+        this.bulletTargetTag = bulletTargetTag;
+    }
+
+    update() {
+        super.update();
+        if (!this.isFiring)
+            return;
+
+        if (this.gameobject.game.time - this.lastFireTime <= this.firingSpeed) {
+            return;
+        }
+        this.lastFireTime = this.gameobject.game.time;
+
+        this.gameobject.emitter.emit("FireShot", {
+            matchId: this.gameobject.game.matchId,
+            id: this.gameobject.id,
+            x: this.gameobject.x,
+            y: this.gameobject.y,
+            angle: this.gameobject.angle
+        });
+
+        this.executeShoot();
+    }
+
+    executeShoot() {
+        const bullet1 = this.createBullet();
+        bullet1.rotateAngle(-30);
+        this.gameobject.game.instantiateEntity(bullet1);
+
+        const bullet2 = this.createBullet();
+        this.gameobject.game.instantiateEntity(bullet2);
+
+        const bullet3 = this.createBullet();
+        bullet3.rotateAngle(30);
+        this.gameobject.game.instantiateEntity(bullet3);
+        
+        if (this.gameobject.isVisible)
+            AudioManager.play(AudioManager.audios.shoot);
+    }
+
+    remoteFire(x, y, angle) {
+        this.executeShoot();
+    }
+
+    createBullet() {
+        var bullet = new Bullet(
+            this.gameobject.game,
+            this.bulletTag,
+            this.gameobject,
+            this.bulletTargetTag,
+            this.bulletSpeed
+        );
+        bullet.destroyAfter(0.8);
+
+        return bullet;
+    }
+}
--- src/images/images.js
+++ src/images/images.js
@@ -1,4 +1,10 @@
 const IMAGES = {
+    playerShip1_blue: require('./playerShip1_blue.png'),
+    playerShip1_green: require('./playerShip1_green.png'),
+    playerShip1_orange: require('./playerShip1_orange.png'),
+    playerShip1_damage1: require('./Damage/playerShip1_damage1.png'),
+    playerShip1_damage2: require('./Damage/playerShip1_damage2.png'),
+    playerShip1_damage3: require('./Damage/playerShip1_damage3.png'),
     playerShip2_blue: require('./playerShip2_blue.png'),
     playerShip2_green: require('./playerShip2_green.png'),
     playerShip2_orange: require('./playerShip2_orange.png'),
